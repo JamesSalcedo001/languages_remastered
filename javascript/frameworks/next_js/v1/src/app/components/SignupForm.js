6a20c5202e15aa6e3dca4e86a7c71bc6e5a4f762
James Salcedo <jamezzzsalcedo17@gmail.com>
2025-03-25 13:53:29 -0500

    Add useForm hook

--- javascript/frameworks/next_js/v1/src/app/components/SignupForm.js
+++ javascript/frameworks/next_js/v1/src/app/components/SignupForm.js
@@ -1,20 +1,27 @@
 "use client";
 
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function SignupForm() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log("Form Submitted: ", data);
+    }
+
     return (
         <div className="max-w-md mx-auto mt-10 p-6 border rounded shadow-md">
             <h2 className="text-xl font-bold mb-4">Signup Form</h2>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <label className="block mb-1">Name: </label>
-                    <input type="text" className="w-full px-3 px-2 border rounded" />
+                    <input {...register("name")} type="text" className="w-full px-3 px-2 border rounded" />
                 </div>
 
-                <div className="mb-b">
+                <div className="mb-4">
                     <label className="block mb-1">Email: </label>
-                    <input type="email" className="ww-full px-3 py-2 border rounded" />
+                    <input {...register("email")} type="email" className="ww-full px-3 py-2 border rounded" />
                 </div>
 
                 <button
