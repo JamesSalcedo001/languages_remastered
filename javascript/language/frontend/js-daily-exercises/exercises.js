3b34dc57fc95adcee2475fefc30b319d1178b1bd
James Salcedo <jamezzzsalcedo17@gmail.com>
2025-05-13 17:56:52 -0500

    Add exercises.js file where questions and answers are stored

---
+++ javascript/language/frontend/js-daily-exercises/exercises.js
@@ -0,0 +1,20 @@
+export const sections = [
+    {
+        title: "Section 1: Loops and Iterations",
+        exercises: [
+            {
+                prompt: "Exercise 1: Print numbers 1 - 5 using a for loop",
+                answer: `for (let i = 1; i <= 5; i++) {\n console.log(i);\n}`
+            }
+        ]
+    },
+    {
+        title: "Section 2: Arrays",
+        exercises: [
+            {
+                prompt: "Exercise 1: Create an array of 5 colors and log it",
+                answer: `const colors = ["red", "blue", "green", "yellow", "purple"];\nconsole.log(colors);`
+            }
+        ]
+    }
+]
