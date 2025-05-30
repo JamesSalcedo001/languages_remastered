f8d8934c6f5bafbf59c11a432ad69f72dafaa583
James Salcedo <jamezzzsalcedo17@gmail.com>
2025-05-29 23:47:25 -0500

    Add new directory js-ex-2

---
+++ javascript/language/frontend/js-ex-2/index.js
@@ -0,0 +1,6 @@
+// let title = document.createElement("h1");
+// let container = document.createElement("div");
+// document.body.appendChild(container);
+// container.appendChild(title);
+
+console.log("Hello")
