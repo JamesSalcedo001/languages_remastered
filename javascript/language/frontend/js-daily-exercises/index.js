3cd3baf7e4a6dc3a4e1cd306c621cf4f341f9156
James Salcedo <jamezzzsalcedo17@gmail.com>
2025-05-30 01:39:52 -0500

    Add PWA functionality

--- javascript/language/frontend/js-daily-exercises/index.js
+++ javascript/language/frontend/js-daily-exercises/index.js
@@ -74,3 +74,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     renderExercise();
 })
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then((reg) => {
+        console.log('✅ Service Worker registered:', reg.scope);
+      })
+      .catch((err) => {
+        console.error('❌ Service Worker registration failed:', err);
+      });
+  });
+}
